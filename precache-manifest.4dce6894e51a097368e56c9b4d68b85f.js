6376bd4a3232b16a2439b9cee39bbcdbcf81f2a6
simyeongryu <rsm0503@gmail.com>
2020-05-24 17:32:16 +0900

    Updates

---
+++ precache-manifest.4dce6894e51a097368e56c9b4d68b85f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfc02e3590730ddc876567a5d0712b03",
+    "url": "/github.io/fresh-code-test/index.html"
+  },
+  {
+    "revision": "e2550d424c5fc1609e09",
+    "url": "/github.io/fresh-code-test/static/css/2.57b6e686.chunk.css"
+  },
+  {
+    "revision": "e2550d424c5fc1609e09",
+    "url": "/github.io/fresh-code-test/static/js/2.d8db32ea.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/github.io/fresh-code-test/static/js/2.d8db32ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26b34d62d43343bdd6a8",
+    "url": "/github.io/fresh-code-test/static/js/main.1fb4301d.chunk.js"
+  },
+  {
+    "revision": "af9bca9e77dc5a37bf1c",
+    "url": "/github.io/fresh-code-test/static/js/runtime-main.4bf86e0a.js"
+  }
+]);
